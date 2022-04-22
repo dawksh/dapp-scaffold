f193c6e28f9376e992b29936231b3c3f4c950305
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2022-04-22 16:20:27 +0530

    feat: init mp-sdk

--- src/components/SendTransaction.tsx
+++ src/components/SendTransaction.tsx
@@ -1,53 +1,92 @@
-import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { Keypair, SystemProgram, Transaction, TransactionSignature } from '@solana/web3.js';
-import { FC, useCallback } from 'react';
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+	Keypair,
+	SystemProgram,
+	Transaction,
+	TransactionSignature,
+	PublicKey,
+} from "@solana/web3.js";
+import { FC, useCallback } from "react";
 import { notify } from "../utils/notifications";
+import { createInitializeHostInstruction, PROGRAM_ID } from "metapass-sdk";
+import * as anchor from "@project-serum/anchor";
 
 export const SendTransaction: FC = () => {
-    const { connection } = useConnection();
-    const { publicKey, sendTransaction } = useWallet();
+	const { connection } = useConnection();
+	const { publicKey, sendTransaction } = useWallet();
 
-    const onClick = useCallback(async () => {
-        if (!publicKey) {
-            notify({ type: 'error', message: `Wallet not connected!` });
-            console.log('error', `Send Transaction: Wallet not connected!`);
-            return;
-        }
+	let hostBump;
+	let hostPDA;
 
-        let signature: TransactionSignature = '';
-        try {
-            const transaction = new Transaction().add(
-                SystemProgram.transfer({
-                    fromPubkey: publicKey,
-                    toPubkey: Keypair.generate().publicKey,
-                    lamports: 1,
-                })
-            );
+	async function initHostPDA() {
+		const [hostPDA, hostBump] = await PublicKey.findProgramAddress(
+			[
+				anchor.utils.bytes.utf8.encode("event-host-key"),
+				publicKey.toBuffer(),
+			],
+			PROGRAM_ID
+		);
+	}
 
-            signature = await sendTransaction(transaction, connection);
+	if (typeof window == "undefined") {
+		initHostPDA();
+	}
 
-            await connection.confirmTransaction(signature, 'confirmed');
-            notify({ type: 'success', message: 'Transaction successful!', txid: signature });
-        } catch (error: any) {
-            notify({ type: 'error', message: `Transaction failed!`, description: error?.message, txid: signature });
-            console.log('error', `Transaction failed! ${error?.message}`, signature);
-            return;
-        }
-    }, [publicKey, notify, connection, sendTransaction]);
+	const onClick = useCallback(async () => {
+		if (!publicKey) {
+			notify({ type: "error", message: `Wallet not connected!` });
+			console.log("error", `Send Transaction: Wallet not connected!`);
+			return;
+		}
 
-    return (
-        <div>
-            <button
-                className="group w-60 m-2 btn animate-pulse disabled:animate-none bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ... "
-                onClick={onClick} disabled={!publicKey}
-            >
-                <div className="hidden group-disabled:block ">
-                    Wallet not connected
-                </div>
-                <span className="block group-disabled:hidden" > 
-                    Send Transaction 
-                </span>
-            </button>
-        </div>
-    );
+		let signature: TransactionSignature = "";
+		try {
+			const transaction = new Transaction().add(
+				SystemProgram.transfer({
+					fromPubkey: publicKey,
+					toPubkey: Keypair.generate().publicKey,
+					lamports: 1,
+				})
+			);
+
+			signature = await sendTransaction(transaction, connection);
+
+			await connection.confirmTransaction(signature, "confirmed");
+			notify({
+				type: "success",
+				message: "Transaction successful!",
+				txid: signature,
+			});
+		} catch (error: any) {
+			notify({
+				type: "error",
+				message: `Transaction failed!`,
+				description: error?.message,
+				txid: signature,
+			});
+			console.log(
+				"error",
+				`Transaction failed! ${error?.message}`,
+				signature
+			);
+			return;
+		}
+	}, [publicKey, notify, connection, sendTransaction]);
+
+	return (
+		<div>
+			<button
+				className="group w-60 m-2 btn animate-pulse disabled:animate-none bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ... "
+				onClick={onClick}
+				disabled={!publicKey}
+			>
+				<div className="hidden group-disabled:block ">
+					Wallet not connected
+				</div>
+				<span className="block group-disabled:hidden">
+					Send Transaction
+				</span>
+			</button>
+		</div>
+	);
 };
